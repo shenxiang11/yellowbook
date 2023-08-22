3f43c37364e258f1b5c637f6bb8b6e50a0ad1462
shenxiang <shenxiang@foursmile001.com>
2023-08-22 14:14:41 +0800

    feat: 增加了 wrk 的 lua

---
+++ wrk/login.lua
@@ -0,0 +1,3 @@
+wrk.method="POST"
+wrk.headers["Content-Type"] = "application/json"
+wrk.body='{"email":"863461785@qq.com", "password": "hello@world#123"}'
---
+++ wrk/profile.lua
@@ -0,0 +1,4 @@
+wrk.method="GET"
+wrk.headers["Content-Type"] = "application/json"
+wrk.headers["User-Agent"] = "PostmanRuntime/7.32.3"
+wrk.headers["Authorization"]="Bearer "
