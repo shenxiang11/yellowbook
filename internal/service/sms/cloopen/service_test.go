c83c00770eb6d56454e2267485f6525e62f64cda
shenxiang <shenxiang@foursmile001.com>
2023-09-05 19:30:56 +0800

    feat: test

--- internal/service/sms/cloopen/service_test.go
+++ internal/service/sms/cloopen/service_test.go
@@ -1,28 +1,35 @@
 package cloopen
 
+import (
+	"context"
+	"github.com/cloopen/go-sms-sdk/cloopen"
+	"testing"
+	"yellowbook/internal/service/sms"
+)
+
 // 真的会发短信，单元测试时注释掉
 
-//func TestCloopen(t *testing.T) {
-//	cfg := cloopen.DefaultConfig().
-//		WithAPIAccount("8aaf07087fe90a32017ff389d6ac01bb").
-//		WithAPIToken("a1c23065a7d847c384d719ad240f6384")
-//
-//	client := cloopen.NewJsonClient(cfg)
-//
-//	s := NewService(client)
-//
-//	err := s.Send(context.TODO(), "1", []sms.NamedArg{
-//		{
-//			Name: "1",
-//			Val:  "1234",
-//		},
-//		{
-//			Name: "2",
-//			Val:  "25444444",
-//		},
-//	}, "18616154465")
-//
-//	if err != nil {
-//		t.Errorf("Error %v", err)
-//	}
-//}
+func TestCloopen(t *testing.T) {
+	cfg := cloopen.DefaultConfig().
+		WithAPIAccount("8aaf07087fe90a32017ff389d6ac01bb").
+		WithAPIToken("a1c23065a7d847c384d719ad240f6384")
+
+	client := cloopen.NewJsonClient(cfg)
+
+	s := NewService(client)
+
+	err := s.Send(context.Background(), "1", []sms.NamedArg{
+		{
+			Name: "1",
+			Val:  "1234",
+		},
+		{
+			Name: "2",
+			Val:  "25444444",
+		},
+	}, "18616154465")
+
+	if err != nil {
+		t.Errorf("Error %v", err)
+	}
+}
--- makefile
+++ makefile
@@ -7,3 +7,4 @@ mock:
 	@/Users/fs/go/bin/mockgen -source=./internal/repository/code.go -package=repomocks -destination=./internal/repository/mocks/code.mock.go
 
 	@/Users/fs/go/bin/mockgen -source=./internal/service/sms/types.go -package=smsmocks -destination=./internal/service/sms/mocks/types.mock.go
+	@/Users/fs/go/bin/mockgen -source=./internal/service/sms/cloopen/service.go -package=cloopenmocks -destination=./internal/service/sms/cloopen/mocks/service.mock.go
